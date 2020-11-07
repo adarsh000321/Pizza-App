d4e887cbd3b09eae6aa26559c6bd1d31fc9cde09
8899871637adarsh <8899871637adarsh@gmail.com>
2020-11-07 15:05:21 +0530

    Pizza

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Hero from "./components/Hero";
+import {BrowserRouter} from "react-router-dom";
+import {GlobalStyle} from "./globalStyles";
+import Product from "./components/Product";
+import Feature from "./components/Feature";
+import {productData, productDataTwo} from "./components/Product/data";
+import Footer from "./components/Footer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = ()=>{
+    return (
+        <BrowserRouter>
+            <GlobalStyle />
+            <Hero />
+            <Product heading = "Choose your favourite" data = {productData}/>
+            <Feature />
+            <Product heading = "Sweet Treats for You" data = {productDataTwo}/>
+            <Footer />
+        </BrowserRouter>
+    )
 }
 
 export default App;
---
+++ src/components/Product/ProductElements.js
@@ -0,0 +1,70 @@
+import styled from "styled-components";
+
+export const ProductContainer = styled.div`
+    width: 100vw;
+    min-height: 100vh;
+    padding: 5rem calc((100vw - 1300px)/2);
+    background: #150f0f;
+    color: #fff;
+`
+
+export const ProductWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+    margin: 0 auto;
+`
+
+export const ProductCard = styled.div`
+    margin: 0 2rem;
+    line-height: 2rem;
+    width: 300px;
+`
+
+export const ProductImg = styled.img`
+    height: 300px;
+    min-width: 300px;
+    max-width: 100%;
+    box-shadow: 8px 8px #fdc500;
+` 
+export const ProductHeading = styled.div`
+    font-size: clamp(2rem, 2.5vw, 3rem);
+    text-align:center;
+    margin-bottom: 5rem; 
+` 
+export const ProductTitle = styled.div`
+    font-weight: 400;
+    font-size: 1.5rem;
+` 
+export const ProductInfo = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    text-align:center;
+    padding: 2rem;
+`  
+
+export const ProductDesc = styled.div`
+    margin-bottom: 1rem;
+`
+
+export const ProductPrice = styled.div`
+    margin-bottom: 1rem;
+    font-size: 2rem;
+`
+export const ProductButton = styled.div`
+    font-size: 1rem;
+    padding: 1rem 4rem;
+    border:none;
+    background:#e31837;
+    color: #fff;
+    transition: 0.2s ease-out;
+
+    &:hover {
+        cursor: pointer;
+        transition: 0.2s ease-out;
+        color: #000;
+        background: #ffc500;
+    }
+`
